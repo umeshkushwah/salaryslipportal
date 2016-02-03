6e1dca6451406ed50019bfe781fecf84786c0e2f
Umesh <ukushwah@grepruby.com>
2016-02-03 16:51:15 +0530

    added changes in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
 #
- Organization.create(name: "GrepRuby", city: "Indore", phone: 07312363363, address: "Tower 61, 1st floor,Bhanwarkuaa", state: "Madhya Pradesh", country: "India", pin_code: 452001)
- employee = Employee.create(first_name: "HR", address: "191-sector A", city: "Indore", pin_code: 452009, dob: '10-10-1990', degignation: "HR", gender: "male", date_of_joining: '12-12-2015', organization_id: 1, password: "123456789", password_confirmation:"123456789", email: "hr@yopmail.com")
+ Organization.create(name: "GrepRuby", city: "Indore", phone: 07312363363, address: "Tower 61, 1st floor,Bhanwarkuaa", state: "Madhya Pradesh", country: "India", pin_code: 452001, domain: "whispering-beyond-80734.herokuapp.com")
+ employee = Employee.create(first_name: "HR-", last_name: "GrepRuby" address: "191-sector A", city: "Indore", pin_code: 452009, dob: '10-10-1990', degignation: "HR", gender: "male", date_of_joining: '12-12-2015', organization_id: 1, password: "123456789", password_confirmation:"123456789", email: "hr@yopmail.com")
  employee.create_role(name: "admin")
 
 
